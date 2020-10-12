2c9f2d024e3e905d037c9838def64ecceff4e4d3
DeltaHalo <36852367+DeltaHalo@users.noreply.github.com>
2020-10-12 11:48:26 +0200

    Update PA.swift

--- Sources/FatturaElettronica/PA.swift
+++ Sources/FatturaElettronica/PA.swift
@@ -4,26 +4,6 @@
 
 import Foundation
 
-extension Array where Element: Codable {
-    init(from decoder: Decoder) throws {
-        
-        if let many = try? decoder.singleValueContainer().decode([Element].self){
-            self = many
-            return
-        }
-        
-        if let one = try? decoder.singleValueContainer().decode(Element.self){
-            self = [one]
-            return
-        }
-        
-        throw DecodingError.missingValue
-    }
-    enum DecodingError: Error {
-        case missingValue
-    }
-}
-
 // MARK: - Entità in comune
 
 public struct Indirizzo: Codable {
