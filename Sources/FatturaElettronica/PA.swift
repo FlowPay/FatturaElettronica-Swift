5d315b18c07ca698efaef9add707310b9fbfffc9
Matteo Franceschi <matteo.franceschi@flowpay.it>
2021-04-27 00:36:37 +0200

    corretto piccola incongruenza

--- Sources/FatturaElettronica/PA.swift
+++ Sources/FatturaElettronica/PA.swift
@@ -44,7 +44,7 @@ public struct Anagrafica: Codable {
         if let value = self.denominazione{
             return value
         }
-        return "\(self.nome ?? "") \(self.cognome ?? "")"
+        return [self.nome, self.cognome].compactMap { $0 }.joined(separator: " ")
     }
     
     public init(denominazione: String?, nome: String?, cognome: String?, codEORI: String?, titolo: String?) {
